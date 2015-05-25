201c58417a98ac44966f0d5095577f8073058af9
Javier de Pedro López <javier.pedro@mobgen.com>
2015-05-25 15:12:50 +0200

    Initial commit

---
+++ SocketsDemo/app/src/main/java/demo/sockets/mobgen/com/socketsdemo/activities/SocketActivity.java
@@ -0,0 +1,16 @@
+package demo.sockets.mobgen.com.socketsdemo.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by javierdepedrolopez on 25/05/15.
+ */
+public class SocketActivity extends Activity {
+
+	@Override
+	public void onCreate (Bundle savedInstanceState, PersistableBundle persistentState) {
+		super.onCreate(savedInstanceState, persistentState);
+	}
+}
